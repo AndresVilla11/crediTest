4a389bc414b4ce139998b37232ac62d6ae846b33
Andres Villamizar <a.villa11@hotmail.com>
2023-09-22 11:58:56 -0500

    first commit

---
+++ src/test/java/com/credibanco/Test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credibanco.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
